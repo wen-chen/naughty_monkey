a0d096d75c690f36a2ea80fb5272096139d033fb
Chen Wen <chenwen@biochen.com>
2022-01-28 22:29:06 +0800

    Update main.cpp

--- cpp_thread_pool/main.cpp
+++ cpp_thread_pool/main.cpp
@@ -5,7 +5,6 @@ std::mutex mutex_lock;
 
 int task_fun(const std::vector<int>& input_vec, int i) {
   std::this_thread::sleep_for(std::chrono::seconds(1));
-  ;
   mutex_lock.lock();
   std::cout << input_vec[i] << std::endl;
   mutex_lock.unlock();
